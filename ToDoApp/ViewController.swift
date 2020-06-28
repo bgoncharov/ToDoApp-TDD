0804308e0194cb40b5a99022599d50a3b9ef3992
Boris Goncharov <bora4bora@gmail.com>
2020-06-28 12:56:04 -0700

    ToDoApp

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Boris Goncharov on 6/28/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
