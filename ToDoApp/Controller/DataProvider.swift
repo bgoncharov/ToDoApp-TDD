332805eb6d2a31752f26df137aa8e027df81c403
Boris Goncharov <bora4bora@gmail.com>
2020-06-30 17:20:21 -0700

    DataProvider

--- ToDoApp/Controller/DataProvider.swift
+++ ToDoApp/Controller/DataProvider.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class DataProvider: NSObject {
+    var taskManager: TaskManager?
     
 }
 
@@ -18,10 +19,14 @@ extension DataProvider: UITableViewDelegate {
 
 extension DataProvider: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskManager?.tasksCount ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
     }
 }
