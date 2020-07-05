42c7467e8d1c5e1ae9e035b0a9fe1656b6c1c2f9
Boris Goncharov <bora4bora@gmail.com>
2020-07-05 12:19:24 -0700

    DetailViewController

---
+++ ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Boris Goncharov on 7/5/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+}
---
+++ ToDoAppTests/Controller/DetailViewControllerTests.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewControllerTests.swift
+//  ToDoAppTests
+//
+//  Created by Boris Goncharov on 7/5/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class DetailViewControllerTests: XCTestCase {
+    
+    var sut: DetailViewController!
+
+    override func setUpWithError() throws {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        sut = storyboard.instantiateViewController(withIdentifier: String(describing: DetailViewController.self)) as? DetailViewController
+        sut.loadViewIfNeeded()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testHasTitleLable() {
+        XCTAssertNotNil(sut.titleLabel)
+        XCTAssertTrue(sut.titleLabel.isDescendant(of: sut.view))
+    }
+    
+    func testHasDescriptionLable() {
+        
+        XCTAssertNotNil(sut.descriptionLabel)
+        XCTAssertTrue(sut.descriptionLabel.isDescendant(of: sut.view))
+    }
+    
+}
