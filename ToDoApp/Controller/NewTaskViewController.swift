6b7e9167bf7a2979d0be0bc1ea9f81a528baed2d
Boris Goncharov <bora4bora@gmail.com>
2020-07-06 11:42:32 -0700

    NewTaskViewController

---
+++ ToDoApp/Controller/NewTaskViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewTaskViewController.swift
+//  ToDoApp
+//
+//  Created by Boris Goncharov on 7/6/20.
+//  Copyright © 2020 Boris Goncharov. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var locationTextField: UITextField!
+    @IBOutlet var dateTextField: UITextField!
+    @IBOutlet var adressTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
+    @IBOutlet var saveButton: UIButton!
+    @IBOutlet var cancelButton: UIButton!
+}
