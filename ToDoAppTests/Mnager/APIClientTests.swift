243f553b80db04c066f28de4469bddf763f44103
Boris Goncharov <bora4bora@gmail.com>
2020-07-08 14:50:58 -0700

    testSuccessfulLoginCreatesToken

--- ToDoAppTests/Mnager/APIClientTests.swift
+++ ToDoAppTests/Mnager/APIClientTests.swift
@@ -15,7 +15,7 @@ class APIClientTests: XCTestCase {
     var mockURLSession: MockURLSession!
 
     override func setUpWithError() throws {
-        mockURLSession = MockURLSession()
+        mockURLSession = MockURLSession(data: nil, urlResponse: nil, responseError: nil)
         sut = APIClient()
         sut.urlSession = mockURLSession
     }
@@ -39,7 +39,7 @@ class APIClientTests: XCTestCase {
         XCTAssertEqual(mockURLSession.urlComponents?.path, "/login")
     }
     
-    func testLoginUsesExpectedQuery() {
+    func testLoginUsesExpectedQueryParameters() {
         userLogin()
         
         guard let queryItems = mockURLSession.urlComponents?.queryItems else {
@@ -53,12 +53,31 @@ class APIClientTests: XCTestCase {
         XCTAssertTrue(queryItems.contains(urlQueryItemName))
         XCTAssertTrue(queryItems.contains(urlQueryItemPassword))
     }
+    
+    //token -> Data -> complitionHandler -> DataTask -> urlSession
+    func testSuccessfulLoginCreatesToken() {
+        let jsonDataStub = "{\"token\": \"tokenString\"}".data(using: .utf8)
+        mockURLSession = MockURLSession(data: jsonDataStub, urlResponse: nil, responseError: nil)
+        sut.urlSession = mockURLSession
+        let tokenExpectation = expectation(description: "Token expectation")
+        
+        var caughtToken: String?
+        sut.login(withName: "login", password: "password") { (token, _) in
+            caughtToken = token
+            tokenExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { _ in
+            XCTAssertEqual(caughtToken, "tokenString")
+        }
+    }
 
 }
 
 extension APIClientTests {
+    
     class MockURLSession: URLSessionProtocol {
         var url: URL?
+        private let mockDataTask: MockURLSessionDataTask
         
         var urlComponents: URLComponents? {
             guard let url = url else {
@@ -68,9 +87,41 @@ extension APIClientTests {
             return URLComponents(url: url, resolvingAgainstBaseURL: true)
         }
         
+        init(data: Data?, urlResponse: URLResponse?, responseError: Error?) {
+            mockDataTask = MockURLSessionDataTask(data: data, urlResponse: urlResponse, responseError: responseError)
+        }
+        
         func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             self.url = url
-            return URLSession.shared.dataTask(with: url)
+            //return URLSession.shared.dataTask(with: url)
+            mockDataTask.completionHandler = completionHandler
+            return mockDataTask
+        }
+    }
+    
+    class MockURLSessionDataTask: URLSessionDataTask {
+        
+        private let data: Data?
+        private let urlResponse: URLResponse?
+        private let responseError: Error?
+        
+        typealias CompletionHandler = (Data?, URLResponse?, Error?) -> Void
+        var completionHandler: CompletionHandler?
+        
+        init(data: Data?, urlResponse: URLResponse?, responseError: Error?) {
+            self.data = data
+            self.urlResponse = urlResponse
+            self.responseError = responseError
+        }
+        
+        override func resume() {
+            DispatchQueue.main.async {
+                self.completionHandler? (
+                    self.data,
+                    self.urlResponse,
+                    self.responseError
+                )
+            }
         }
     }
 }
